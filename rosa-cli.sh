bfaa336aaeba87810fbe99c644af3a1e974ff80b
Rakesh Kumar Mallam <60370210+rmallam@users.noreply.github.com>
2024-08-13 13:46:13 +1000

    Update rosa-cli.sh

--- rosa-cli.sh
+++ rosa-cli.sh
@@ -1,3 +1,3 @@
-rosa create idp --cluster=rosa-kzp2h --type htpasswd  --from-file ./passwordfile --name Hackathon
+rosa create idp --cluster=`rosa list clusters | awk -F " " '{print $2}' | grep -v NAME` --type htpasswd  --from-file ./passwordfile --name Hackathon
 # delete the users from the clusters if they are already bound to any other IDP, in this case IDP name is hackathon
 # oc delete user cluster-admin admin red blue green yellow orange purple hack-admin
